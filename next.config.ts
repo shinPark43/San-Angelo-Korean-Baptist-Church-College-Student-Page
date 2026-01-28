62c12b99a1e6c072ec1c25043280768f172c615e
Shin Park <spark43@angelo.edu>
2026-01-28 15:15:12 -0600

    Added files for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/San-Angelo-Korean-Baptist-Church-College-Student-Page",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
